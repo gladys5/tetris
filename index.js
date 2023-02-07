ed9ad6e17ffd4e6f2a3f5417ce91a71632ee9768
gladys5 <yumanji@live.com>
2023-02-06 23:24:25 -0700

    inicializacion de variables

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-//Hay que abrir los demás números que están al rededor
-for (let i = -1; i <= 1; i++) {
-  for (let j = -1; j <= 1; j++) {
-    console.log(i, j)
-  }
-}
+const zTetromino = "secondShape"
+const oTetromino = "thirdShape"
+const iTetromino = "fourthShape"
+const tTetromino = "fifthShape"
+
+
+const tetrominoes =[lTetromino,zTetromino,oTetromino,tTetromino]
+
+console.log(tetrominoes[5])
+
+document.addEventListener('DOMContentLoaded',(()=>{
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll(".grid div"))
+let ScoreDisplay =document.querySelector("#score")
+let StartBtn = document.querySelector("#start-button")
+const width =10
+console.log(squares)
+
+}))
+
+
+
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+]
+
+
+
+
+
+
+
