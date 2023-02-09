c3492636a56da86165104ba2be27afc1e835bb33
gladys5 <yumanji@live.com>
2023-02-08 23:00:23 -0700

    primera posicion random

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let score = 0
   const colors = ["orange", "red", "purple", "green", "blue"]
 
-  //The Tetrominoes
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
@@ -52,5 +51,28 @@ document.addEventListener("DOMContentLoaded", () => {
     oTetromino,
     iTetromino,
   ]
+  
 
 })
+const theTetrominoes = [
+  lTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+]
+
+let currentPosition = 4
+let currentRotation = 0
+
+console.log(theTetrominoes[0][0])
+
+let random = Math.floor(Math.random() * theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino")
+    squares[currentPosition + index].style.backgroundColor = colors[random]
+  })
+}
